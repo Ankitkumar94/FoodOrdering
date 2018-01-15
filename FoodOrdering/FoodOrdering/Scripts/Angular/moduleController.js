db64f6decd5a7c37ecfdbec70a09c2089f75c2ea
Ankit Kumar <ankit.kumar@cybergindia.local>
2018-01-15 08:34:30 +0530

    Added Login page layout

---
+++ FoodOrdering/FoodOrdering/Scripts/Angular/moduleController.js
@@ -0,0 +1,26 @@
+﻿var LoginModule = angular.module('LoginPage', []);
+
+LoginModule.controller('LoginController', function ($scope, LoginService) {
+    $scope.login = function (form) {
+
+    }
+})
+    .factory('LoginService', function ($http) {
+        var fac = {};
+        fac.GetUser = function (d) {
+            return $http({
+                url: '/Home/UserLogin',
+                method: 'POST',
+                data: JSON.stringify(d),
+                headers: { 'content-type': 'application/json' }
+            })
+        };
+        return fac;
+    })
+
+
+LoginModule.controller('RegisterController', function ($scope) {
+    $scope.register = function (form) {
+
+    }
+});
